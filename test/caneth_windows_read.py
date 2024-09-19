cbcfd98520a6ac90093b28c5ed06078622cfb1d0
XKrug Bubeck <bubeck@xkrug.com>
2024-09-19 13:21:18 +0200

    Added Tx frame for the test

--- test/caneth_windows_read.py
+++ test/caneth_windows_read.py
@@ -7,5 +7,7 @@ notifier1 = can.Notifier(bus1, [can.Printer()])
 
 while True:
     time.sleep(1.0)
+    message = can.Message(arbitration_id=0x123, data=[1, 2, 3])
+    bus1.send(message)
 
 bus1.shutdown()
